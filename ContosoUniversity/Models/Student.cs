93c3f19e7d6847631ad43650f2b62c9b42b9b771
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-08-27 16:15:55 +0200

    Create migrations for column change name and specify validations. Update database.

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace ContosoUniversity.Models
@@ -9,15 +10,30 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
 
+        [Required]
         [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
